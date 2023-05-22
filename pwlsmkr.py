3a57566f5f3dd239885caa365920a7b43048f87a
an0mali <dquigz89@gmail.com>
2023-05-22 18:54:18 -0400

    Seperated namegen into own module

--- pwlsmkr.py
+++ pwlsmkr.py
@@ -89,47 +89,6 @@ class Pwlsmkr(object):
             if keyword != 'wordlist':
                 print(keyword + ': ' + str(self.props[keyword]))
 
-    def generate_names(self, amt, incmale=True, incfemale=True, incfirst=True, incmiddle=True, inclast=True):
-        #Generate and return a list of names, might be useful for a target that utilizes names in passwords
-
-        setgender = 'male'
-        alternate_genders = False
-        #Set gender based on args
-        if not (incmale and incfemale):
-            if incmale:
-                setgender = 'male'
-            else:
-                setgender = 'female'
-        else:
-            alternate_genders = True
-            
-        
-        #Generate names
-        for x in range(0, amt):
-            #Generate first name based on gender arg
-            name = ''
-
-            if incfirst:
-                name = names.get_first_name(gender=setgender)
-            #Generate middle name based on arg
-            if incmiddle:
-                while True:
-                    midname = names.get_first_name(gender=setgender)
-                    if midname != name:
-                        name += midname
-                        break
-           
-            if inclast:
-                name += names.get_last_name()
-
-            if not name in self.props['names']:
-                self.props['names'].append(name)
-                if alternate_genders:
-                    if setgender == 'male':
-                        setgender = 'female'
-                    else:
-                        setgender = 'male'
-
 #Create argument parser
 parser = argparse.ArgumentParser(
     prog='pwlsmkr',
